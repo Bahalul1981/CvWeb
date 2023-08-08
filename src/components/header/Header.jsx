67c74c8b5189d479d04c7cf2425bbfe02be12b94
Dawn Amin Ahmed <>
2023-08-08 23:01:13 +0200

    hello

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -23,7 +23,7 @@ export const Header = () => {
           </li>
           <li>
             <NavLink to="/skills" onClick={toggleNav}>
-              Kompetens
+              Kompetenss
             </NavLink>
           </li>
           <li>
